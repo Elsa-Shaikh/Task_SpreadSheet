70a64e08cd9fd1560ff20bc044585167eb892176
Elsa <elsashaikhpk@gmail.com>
2023-08-20 21:08:55 +0500

    Spreadsheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { SpreadsheetComponent } from '@syncfusion/ej2-react-spreadsheet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <SpreadsheetComponent allowOpen={true} allowSave={true} 
+     openUrl='https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open'
+     saveUrl='https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save'
+     >
+
+     </SpreadsheetComponent>
+
     </div>
   );
 }
